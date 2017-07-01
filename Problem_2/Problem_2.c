b97715bcd404c22103e83367014fec62cea993c2
Daniel Olbrys <olbrys.daniel@gmail.com>
2017-07-01 19:08:35 -0400

    Add problem 2

---
+++ Problem_2/Problem_2.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+
+static int fibNums[34] = {0};
+
+int fibbonaci(int x);
+
+int main(int argc, char const *argv[]) {
+  int i = 0, sum = 0;
+  fibNums[0] = 1;
+  fibNums[1] = 1;
+  (void)fibbonaci(33);
+
+  for(i=2; i < 34; i+=3) {
+    sum += fibNums[i];
+  }
+
+  printf("%d\n", sum);
+  return 0;
+}
+
+int fibbonaci(int x) {
+  if(x == 0) return 1;
+  if(x == 1) return 1;
+  if(fibNums[x] != 0) return fibNums[x];
+
+  fibNums[x] = fibbonaci(x-1) + fibbonaci(x-2);
+
+  return fibNums[x];
+}
