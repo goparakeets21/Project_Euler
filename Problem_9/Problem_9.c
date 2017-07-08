6506bf4e205944a671ddcc148a88273ddce92409
Daniel Olbrys <olbrys.daniel@gmail.com>
2017-07-08 18:52:21 -0400

    Update format for Problem 9

--- Problem_9/Problem_9.c
+++ Problem_9/Problem_9.c
@@ -1,31 +1,40 @@
 #include "stdio.h"
 #include "stdbool.h"
 
-bool checkPythagoreanTriplet (int a, int b, int c);
+bool checkPythagoreanTriplet(int a, int b, int c);
 
-bool checkPythagoreanTriplet (int a, int b, int c) {
-    if((a*a) + (b*b) == (c*c)) {
-        return true;
-    } else {
-        return false;
+bool checkPythagoreanTriplet(int a, int b, int c)
+{
+  if ((a * a) + (b * b) == (c * c))
+    {
+      return true;
+    }
+  else
+    {
+      return false;
     }
 }
 
-int main(int argc, char const *argv[]) {
-    int i;
-    int j;
-    int k;
-    
-    for(i=1; i < 1000; i++) {
-        for(j=i; j < (1000 - i); j++) {
-            for(k=j; k < (1000 - j); k++){
-                if(checkPythagoreanTriplet(i,j,k) && (i + j + k) == 1000) {
-                    printf("%d %d %d\n", i, j, k);
-                    printf("%d\n", (i*j*k));
-                    break;
+int main(int argc, char const *argv[])
+{
+  int i;
+  int j;
+  int k;
+
+  for (i = 1; i < 1000; i++)
+    {
+      for (j = i; j < (1000 - i); j++)
+        {
+          for (k = j; k < (1000 - j); k++)
+            {
+              if (checkPythagoreanTriplet(i, j, k) && (i + j + k) == 1000)
+                {
+                  printf("%d %d %d\n", i, j, k);
+                  printf("%d\n", (i * j * k));
+                  break;
                 }
             }
         }
     }
-    return 0;
+  return 0;
 }
