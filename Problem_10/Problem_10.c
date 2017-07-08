ad4392572d99967eac27f6a50738a67072d26378
Daniel Olbrys <olbrys.daniel@gmail.com>
2017-07-08 17:02:15 -0400

    Solution to Problem 10

---
+++ Problem_10/Problem_10.c
@@ -0,0 +1,48 @@
+#include "stdio.h"
+#include "stdbool.h"
+#include "string.h"
+#include "stdint.h"
+#include "inttypes.h"
+
+#ifndef MAX_INT
+#define MAX_INT 2000000u
+#endif
+
+static bool prime[MAX_INT] = {true};
+
+void SieveOfEratosthenes(void);
+
+void SieveOfEratosthenes(void) {
+  uint64_t p, i;
+  memset(prime, true, sizeof(prime));
+
+  prime[0] = false;
+  prime[1] = false;
+
+  for (p=2; p*p<=MAX_INT; p++)
+    {
+        // If prime[p] is not changed, then it is a prime
+        if (prime[p] == true)
+        {
+            // Update all multiples of p
+            for (i=p*2; i<=MAX_INT; i += p)
+                prime[i] = false;
+        }
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    uint64_t sum = 0u;
+    uint32_t i;
+    SieveOfEratosthenes();
+
+    for(i = 0u; i < MAX_INT; ++i) {
+        if(prime[i] == true) {
+            sum += i;
+        }
+    }
+
+    printf("%" PRIu64 "\n", sum);
+
+    return 0;
+}
